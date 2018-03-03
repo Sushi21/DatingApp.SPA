af1f5b7baa46626f9bed70200aca79466399a82d
Sushi21 <yannick.dufils@gmail.com>
2018-03-03 15:03:30 -0400

    Error Handling SPA

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       console.log('login successfully');
     }, error => {
-      console.log('fail login');
+      console.log(error);
     });
   }
 
