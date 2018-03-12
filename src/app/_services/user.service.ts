64569275681bc4a45a986b6f63b734d7cda71e5b
Sushi21 <yannick.dufils@gmail.com>
2018-03-11 21:30:03 -0300

    adding likes functionality

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -15,12 +15,20 @@ export class UserService {
 
   constructor(private authHttp: AuthHttp) {}
 
-  getUsers(page?: number, itemsPerPage?: number, userParams?: any) {
+  getUsers(page?: number, itemsPerPage?: number, userParams?: any, likesParams?: string) {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
     let queryString = '?';
 
     if (page != null && itemsPerPage != null) {
       queryString += 'pageNumber=' + page + '&pageSize=' + itemsPerPage + '&';
+    }
+
+    if (likesParams === 'Likers') {
+      queryString += 'Likers=true&';
+    }
+
+    if (likesParams === 'Likees') {
+      queryString += 'Likees=true&';
     }
 
     if (userParams != null) {
@@ -68,7 +76,14 @@ export class UserService {
     return this.authHttp.delete(this.baseUrl + 'users/' + userId + '/photos/' + id).catch(this.handleError);
   }
 
+  sendLike(id: number, recipientId: number) {
+    return this.authHttp.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {}).catch(this.handleError);
+  }
+
   private handleError(error: any) {
+    if (error.status === 400) {
+      return Observable.throw(error._body);
+    }
     const applicationError = error.headers.get('Application-Error');
     if (applicationError) {
       return Observable.throw(applicationError);
