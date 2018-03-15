c125087482520dfb2c1407526707098bf965eb8c
Sushi21 <yannick.dufils@gmail.com>
2018-03-14 21:11:17 -0300

    Messages functionality

--- src/app/_resolvers/message.resolver.ts
+++ src/app/_resolvers/message.resolver.ts
@@ -6,19 +6,24 @@ import { AlertifyService } from '../_services/alertify.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
+import { Message } from '../_models/message';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
-export class MemberListResolver implements Resolve<User[]> {
+export class MessagesResolver implements Resolve<Message[]> {
   pageSize = 5;
   pageNumber = 1;
+  messageContainer = 'Unread';
   constructor(
     private userService: UserService,
     private router: Router,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private authService: AuthService
   ) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-    return this.userService.getUsers(this.pageNumber, this.pageSize).catch(error => {
+  resolve(route: ActivatedRouteSnapshot): Observable<Message[]> {
+    return this.userService.getMessages(this.authService.decodedToken.nameid,
+      this.pageNumber, this.pageSize, this.messageContainer).catch(error => {
       this.alertify.error('Problem retrieving data');
       this.router.navigate(['/members']);
       return Observable.of(null);
